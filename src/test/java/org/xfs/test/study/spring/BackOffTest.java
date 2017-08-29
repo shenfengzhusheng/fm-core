80ca9f9bbd7ca8399bde044f74c161de96b1f60e
神风逐胜 <xixingyingzhongdui@gmail>
2017-08-29 16:58:19 +0800

    deal warn

--- src/test/java/org/xfs/test/study/spring/BackOffTest.java
+++ src/test/java/org/xfs/test/study/spring/BackOffTest.java
@@ -14,6 +14,7 @@ import org.springframework.util.backoff.FixedBackOff;
  * @author Jeken.Liu
  *
  */
+@SuppressWarnings("deprecation")
 public class BackOffTest {
     // @Test
     public void testFixedBackOff() {
