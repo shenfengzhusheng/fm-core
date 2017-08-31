bb3dd62e4b03be2bd1c90df9db588cffca60a3f6
xixingyingzhongdui@gmail.com <xixingyingzhongdui@gmail.com>
2017-08-31 21:35:54 +0800

    e

--- src/main/java/org/xfs/core/business/order/service/OrderRepository.java
+++ src/main/java/org/xfs/core/business/order/service/OrderRepository.java
@@ -18,7 +18,7 @@ public interface OrderRepository {
     public Order getOrder(String orderId);
 
     /**
-     * 获取全部订单dd 
+     * 获取全部订单dds 
      * project:fm-core
      * @return 
      * author:xifengshan 
